849e11c0ed80ddfc59530b24699fd5245293fc7c
milkocondoriparedes <crybardack@gmail.com>
2017-09-29 14:03:47 -0500

    Enviando Practica1

---
+++ Practica1/Ejercicio2.R
@@ -0,0 +1,5 @@
+#Cristhian Condori Paredes 20131422K
+
+#suma
+
+1+prod(seq(2,3,by=2))/prod(seq(3,3,by=2))+prod(seq(2,5,by=2))/prod(seq(3,5,by=2))
---
+++ Practica1/Ejercicio3.R
@@ -0,0 +1,8 @@
+#Cristhian Condori Paredes --
+
+#probabilidades de formacion de los 6 niños
+
+#combinacion de 6=6!,probabilidad de esten ordenados alfaeticamente 1
+1/factorial(6)
+#porbabilidad de q esten ordenados de 3 y luego los otros 3
+choose(3,6)/2

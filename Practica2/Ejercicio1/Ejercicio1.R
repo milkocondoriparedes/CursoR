b4a9b997e351f1bc9253db62a05cfa43b2121f90
Llampi <ll_elias@hotmail.com>
2018-05-30 00:53:58 -0500

    Update Ejercicio1.R

--- Practica2/Ejercicio1/Ejercicio1.R
+++ Practica2/Ejercicio1/Ejercicio1.R
@@ -1,1 +1,10 @@
 
+##Preg1
+
+#Preg 1.a
+#Si el paquete methods se carga al iniciar de modo automatico lo podemos encontrar con search(), que devuelve una trayectoria de busqueda con listas, bibliotecas y hojas de datos que se estan usando
+#Con esto en cuenta usamos funciones de busqueda y comparacion (match) para encontrar los 20 primeros elementos   
+search()
+ls(match("package:methods",search()))[1:20]
+#Y para el numero de elementos del paquete:
+length(ls(match("package:methods",search())))

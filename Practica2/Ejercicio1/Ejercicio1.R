3f08be7d687bdc201b1fa474f4f09d3a79b694d5
Llampi <ll_elias@hotmail.com>
2018-05-30 22:05:23 -0500

    Update Ejercicio1.R

--- Practica2/Ejercicio1/Ejercicio1.R
+++ Practica2/Ejercicio1/Ejercicio1.R
@@ -9,6 +9,23 @@ ls(match("package:methods",search()))[1:20]
 #Y para el numero de elementos del paquete:
 length(ls(match("package:methods",search())))
 
+#Preg 1.b
+#Las librerias o paquetes a la que pertenece una funcion en especifico se obtiene usando el comando help()
+#Asi para la funcion read.table:
+help("read.table")
+#Se observa que pertenece al paquete utils
+cat("La funcion read.table pertenece al paquete utils")
+
+#Y del mismo modo para las otras funciones:
+help("data")
+cat("La funcion data pertenece al paquete utils")
+#con la funcion matrix:
+help("matrix")
+cat("La funcion matrix pertenece al paquete base")
+#Por ultimo con la funcion jpeg:
+help("jpeg")
+cat("La funcion jpeg pertenece al paquete grDevices")
+
 
 #Preg 1.c
 #ls(match("package:graphics",search())) es una cadena cuyos elementos son los elementos del paquete graphics, con %in% verificamos si la funcion smoothScatter se encuentra en la cadena o no

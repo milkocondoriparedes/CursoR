0fbc22851893c44cc5cb76b433802dc9b3ab4127
milkocondoriparedes <31776730+milkocondoriparedes@users.noreply.github.com>
2018-06-20 16:10:43 -0500

    Terminando Ejercicio1

--- Practica1/Ejercicio1/Ejercicio1.R
+++ Practica1/Ejercicio1/Ejercicio1.R
@@ -1,6 +1,6 @@
 #Nombre: Betty Mendoza Chuquiruna 20150497B
 #        Cristhian Condori Paredes 20131422K
-#        Elias Llampi Aliaga 	20141460B
+#        Elias Llampi Aliaga 20141460B
 
 #Respuesta1:
 

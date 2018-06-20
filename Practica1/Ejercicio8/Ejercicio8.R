ac0b6af642699f1f59c62eb02f851f3bd52dc8af
milkocondoriparedes <31776730+milkocondoriparedes@users.noreply.github.com>
2018-06-19 19:25:12 -0500

    Update Ejercicio8.R

--- Practica1/Ejercicio8/Ejercicio8.R
+++ Practica1/Ejercicio8/Ejercicio8.R
@@ -3,4 +3,33 @@
 
 #Respuesta N8:
  
-#(a)El codigo muestra :
+#(a)El codigo muestra el uso de DATA.FRAME para almacenar en "dframe" el vector "persona","sexo" y "puntuacion":
+persona<-c("Stan","Francine","Steve","Roger","Hayley","Klaus")
+sexo<-factor(c("M","F","M","M","F","M"),levels=c("F","M"))
+puntuacion<-factor(c("Alto","Medio","Bajo","Alto","Medio","Medio"),levels=c("Alto","Medio","Bajo"))
+dframe<-data.frame(persona, sexo, puntuacion)
+dframe
+   persona sexo puntuacion
+1     Stan    M       Alto
+2 Francine    F      Medio
+3    Steve    M       Bajo
+4    Roger    M       Alto
+5   Hayley    F      Medio
+6    Klaus    M      Medio
+
+#(b)El codigo muestra 
+edad<-c(41,41,15,1600,21,60)
+dframe<-data.frame(dframe,edad)
+dframe
+   persona sexo puntuacion edad
+1     Stan    M       Alto   41
+2 Francine    F      Medio   41
+3    Steve    M       Bajo   15
+4    Roger    M       Alto 1600
+5   Hayley    F      Medio   21
+6    Klaus    M      Medio   60
+
+                   
+                   
+                   
+                   

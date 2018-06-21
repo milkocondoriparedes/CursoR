6881b8a81f5e6bbe584576b0a93097314a62d884
milkocondoriparedes <31776730+milkocondoriparedes@users.noreply.github.com>
2018-06-21 16:45:13 -0500

    Falta Ejercicio7

--- Practica2/Ejercicio7/Ejercicio7.R
+++ Practica2/Ejercicio7/Ejercicio7.R
@@ -24,3 +24,49 @@ fun<-function()
 }
 #Probamos los datos
 fun()
+
+fun<-function(cadena1,cadena2)
+{
+  cadena1<-character(10)
+  cadena2<-character(10)
+  contador1<-numeric(1)
+  contador2<-numeric(1)
+  contador3<-numeric(1)
+  for (i in 1 : length(cadena1)) 
+  {
+    if(cadena1[i]=="+"){
+      contador1<-contador1+1
+      }
+    else if (cadena1[i]=="-"){
+      contador2<-contador2+1
+      }
+    else if (cadena1[i]=="?"){
+      contador3<-contador3+1
+      }
+     if(cadena2[i]=="+"){
+      contador1<-contador1-1
+      }
+    else if (cadena2[i]=="-"){
+      contador2<-contador2-1
+      }
+    else if (cadena2[i]=="?"){
+      contador3<-contador3+1
+      }
+    # con esto "contador1" almacenara las veces que el signo + hace falta en la cadena de A
+  }
+  cat("La probabilidad de que A coloque bien la antena es: \n")
+  cat(contador1)
+  cat(contador3)
+  dbinom(contador1,contador3,1/2)
+}
+
+cadena1<-c("+","-","+","-")
+cadena2<-c("+","-","?","?")
+fun(cadena1,cadena2)
+
+cadena1<-c("+","+","-","+","-")
+cadena2<-c("+","-","+","-","+")
+fun()
+
+
+

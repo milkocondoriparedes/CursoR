603df00ce6a5ad5c42011710802aad32e3574a80
Llampi <ll_elias@hotmail.com>
2018-06-23 16:34:08 -0500

    Terminado

--- Practica2/Ejercicio7/Ejercicio7.R
+++ Practica2/Ejercicio7/Ejercicio7.R
@@ -1,3 +1,5 @@
+
+
 ###PREG7
 #Preg7.a
 #La subcadena "??...??" se asemeja a n lanzamiento de monedas, donde "?" puede ser + o -, cada uno con %50 de probabilidad,
@@ -20,53 +22,49 @@ fun<-function()
     # con esto "contador1" almacenara las veces que el signo + hace falta en la cadena de A
   }
   cat("La probabilidad de que A coloque bien la antena es: \n")
-  dbinom(contador1,contador3,1/2)
+  format(round(dbinom(contador1,contador3,1/2),9), nsmall = 9)
 }
-#Probamos los datos
+#Probamos la funcion para el ejemplo 1
 fun()
+++-+-
++-+-+
+  
+#Probamos la funcion para el ejemplo 2
+fun()
++-+-
++-??
+  
+# si queremos ingresar nuevas prebas, simplemente escribimos fun() y tipeamos las entradas cuando las pida
 
-fun<-function(cadena1,cadena2)
+## parte b de la 7
+funcion2 <-function()
 {
-  cadena1<-character(10)
-  cadena2<-character(10)
-  contador1<-numeric(1)
-  contador2<-numeric(1)
-  contador3<-numeric(1)
-  for (i in 1 : length(cadena1)) 
-  {
-    if(cadena1[i]=="+"){
-      contador1<-contador1+1
-      }
-    else if (cadena1[i]=="-"){
-      contador2<-contador2+1
-      }
-    else if (cadena1[i]=="?"){
-      contador3<-contador3+1
-      }
-     if(cadena2[i]=="+"){
-      contador1<-contador1-1
-      }
-    else if (cadena2[i]=="-"){
-      contador2<-contador2-1
-      }
-    else if (cadena2[i]=="?"){
-      contador3<-contador3+1
-      }
-    # con esto "contador1" almacenara las veces que el signo + hace falta en la cadena de A
+    #Almacenamos las sumas de los dados en un vector y el numero de combinaciones de dados que lo hacen posible en otro
+    # notar que una suma de dados y su frecuencia en los vectores 1 y 2 respectivamente tienen la misma posicion
+  vector1 <- c(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)
+  vector2 <- c(1,3,6,10,15,21,25,27,27,25,21,15,10,6,3,1)
+  
+  #pedimos la suma que obtvo vilma y lo almacenamos
+  primer_numero<-readline("Ingresar la suma que obtuvo Vilma ")
+  #pedimos la suma que obtvo esther y lo almacenamos
+  segundo_numero<-readline("Ingresar la suma que obtuvo Esther ")
+  #obtenemos el maximo de esos numeros pedidos entre vilma y esther
+  suma_max <- max(segundo_numero,primer_numero)
+  #comparamos ese numero maximo con los elementos del vector1 y obtenemos la posicion
+  posicion <- match(suma_max, vector1)
+  # la varible suma sera el numero de casos favorables que se ira incrementando conforme avance el ciclo for siguiente
+  suma<-0
+  # este ciclo for contara el total de casos favorables()
+  for (i in posicion:length(vector2)) {
+    suma <- suma + vector2[i]
   }
-  cat("La probabilidad de que A coloque bien la antena es: \n")
-  cat(contador1)
-  cat(contador3)
-  dbinom(contador1,contador3,1/2)
+  cat("La probabilidad de que Jessica gane es:  ")
+  cat(suma,"/","216")
 }
+#Si queremos probar los datos del ejemplo:
+funcion2()
+4
+4
 
-cadena1<-c("+","-","+","-")
-cadena2<-c("+","-","?","?")
-fun(cadena1,cadena2)
-
-cadena1<-c("+","+","-","+","-")
-cadena2<-c("+","-","+","-","+")
-fun()
-
-
-
+#Probamos el codigo con un dato ingresado por el usuario
+funcion2()
